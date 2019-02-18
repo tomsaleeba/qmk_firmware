be14a99401ca064514679fe9a58363b6b2e274c8
Tom Saleeba <tom.saleeba@gmail.com>
2019-02-18 15:59:03 +1030

    feat: remove 'hold for control' on Z key of infinity

--- keyboards/ergodox_infinity/keymaps/tom/keymap.c
+++ keyboards/ergodox_infinity/keymaps/tom/keymap.c
@@ -25,7 +25,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|      |           |      |------+------+------+------+------+--------|
  * | Tab    |   A  |   S  |   D  |   F  |   G  |------|           |------|   H  |   J  |   K  |   L  |; / L3|   '    |
  * |--------+------+------+------+------+------|  L1  |           | Meh  |------+------+------+------+------+--------|
- * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
+ * | LShift |   Z  |   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
  *   | Ctrl |Grv/L1| Alt  | Left | Right|                                       | Left | Down | Up   | Right| RGUI |
  *   `----------------------------------'                                       `----------------------------------'
@@ -44,7 +44,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_EQL,      KC_1,            KC_2,    KC_3,    KC_4,   KC_5,              KC_ESC,
         KC_BSLS,     KC_Q,            KC_W,    KC_E,    KC_R,   KC_T,              TG(MDIA),
         KC_TAB,      KC_A,            KC_S,    KC_D,    KC_F,   KC_G,
-        KC_LSFT,     CTL_T(KC_Z),     KC_X,    KC_C,    KC_V,   KC_B,              MO(MDIA),
+        KC_LSFT,     KC_Z,            KC_X,    KC_C,    KC_V,   KC_B,              MO(MDIA),
         KC_LCTL,     LT(SYMB,KC_GRV), KC_LALT, KC_LEFT, KC_RGHT,
                                                                 KC_F2,             KC_LALT,
                                                                                    KC_HOME,
