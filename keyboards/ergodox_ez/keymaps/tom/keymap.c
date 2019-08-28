4dfc9bfc64369fed2a08532f685e425d49b51f6a
Tom Saleeba <tom.saleeba@gmail.com>
2019-08-28 11:22:20 +0930

    feat: unbind keys that won't exist on smaller keyboard in ez base layer

--- keyboards/ergodox_ez/keymaps/tom/keymap.c
+++ keyboards/ergodox_ez/keymaps/tom/keymap.c
@@ -32,14 +32,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  * |--------+------+------+------+------+------|  L1  |           | Swap |------+------+------+------+------+--------|
  * | LShift |Z/Ctrl|   X  |   C  |   V  |   B  |      |           |      |   N  |   M  |   ,  |   .  |//Ctrl| RShift |
  * `--------+------+------+------+------+-------------'           `-------------+------+------+------+------+--------'
- *   | Ctrl |Grv/L1| Alt  | Left | Right|                                       | Left | Down | Up   | Right| RGUI |
+ *   | Ctrl |Grv/L1| Alt  | Left |  L5  |                                       | Left | Down | Up   | Right| RGUI |
  *   `----------------------------------'                                       `----------------------------------'
  *                                        ,-------------.       ,-------------.
- *                                        |  F2  | LAlt |       | Alt  |Ctrl/Esc|
+ *                                        |      |      |       |      |        |
  *                                 ,------|------|------|       |------+--------+------.
- *                                 |      |      | Home |       | PgUp |        |      |
- *                                 | BkSp | Del  |------|       |------| Enter  | Space|
- *                                 |      |      | End  |       | PgDn |        |      |
+ *                                 |      |      |      |       |      |        |      |
+ *                                 | BkSp |      |------|       |------| Enter  | Space|
+ *                                 |      |      |      |       |      |        |      |
  *                                 `--------------------'       `----------------------'
  */
 // If it accepts an argument (i.e, is a function), it doesn't need KC_.
@@ -50,19 +50,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         KC_BSLS,     KC_Q,            KC_W,    KC_E,    KC_R,   KC_T,              TT(SWAP),
         KC_TAB,      KC_A,            KC_S,    KC_D,    KC_F,   KC_G,
         KC_LSFT,     KC_Z,            KC_X,    KC_C,    KC_V,   KC_B,              MO(MDIA),
-        KC_LCTL,     KC_GRV,          KC_LALT, KC_LEFT, MO(NUMB),
-                                                                KC_F2,             KC_LALT,
-                                                                                   KC_HOME,
-                                                        KC_BSPC,KC_DELT,           KC_END,
+        KC_LCTL,     KC_GRV,          KC_LALT, KC_DELT, MO(NUMB),
+                                                                KC_TRNS,           KC_TRNS,
+                                                                                   KC_TRNS,
+                                                        KC_BSPC,KC_TRNS,           KC_TRNS,
         // right hand
         TG(SYMB),    KC_TRNS,         KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,           KC_TRNS,
         KC_LBRC,     KC_Y,            KC_U,    KC_I,    KC_O,   KC_P,              KC_RBRC,
                      KC_H,            KC_J,    KC_K,    KC_L,   LT(MOUS, KC_SCLN), KC_QUOT,
         TT(SWAP),    KC_N,            KC_M,    KC_COMM, KC_DOT, CTL_T(KC_SLSH),    KC_RSFT,
                                       KC_LEFT, KC_DOWN, KC_UP,  KC_RGHT,           KC_RGUI,
-        KC_LALT,     KC_RCTL,
-        KC_PGUP,
-        KC_PGDN,     KC_ENT,          KC_SPC
+        KC_TRNS,     KC_TRNS,
+        KC_TRNS,
+        KC_TRNS,     KC_ENT,          KC_SPC
     ),
 /* Keymap 1: Media and mouse keys
  *
