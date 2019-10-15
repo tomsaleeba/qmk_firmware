5ef3f45affde1fae5f3af6c18ce5f57d60e130d6
Tom Saleeba <tom.saleeba@gmail.com>
2019-10-15 16:32:30 +1030

    feat: more rejigging as I find out what works

--- layouts/community/ergodox/tomsaleeba-minimal/config.h
+++ layouts/community/ergodox/tomsaleeba-minimal/config.h
@@ -1,7 +1,3 @@
 // TODO might need this for ergodox_ez
 // seems 15 causes a bit of chatter on my board
 //#define DEBOUNCE    25
-
-// for permissive hold
-#define PERMISSIVE_HOLD
-#undef IGNORE_MOD_TAP_INTERRUPT
