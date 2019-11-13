7b1a2f22100de166d4a96b7c6290af1aa8e32a0b
Tom Saleeba <tom.saleeba@gmail.com>
2019-11-13 21:03:31 +1030

    feat: fix mouse keys on ergotravel

--- keyboards/ergotravel/keymaps/tom/config.h
+++ keyboards/ergotravel/keymaps/tom/config.h
@@ -35,4 +35,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define RGBLIGHT_SAT_STEP 8
 #define RGBLIGHT_VAL_STEP 8
 
-
+#define MOUSEKEY_INTERVAL       20
+#define MOUSEKEY_DELAY          0
+#define MOUSEKEY_TIME_TO_MAX    60
+#define MOUSEKEY_MAX_SPEED      7
+#define MOUSEKEY_WHEEL_DELAY 0
