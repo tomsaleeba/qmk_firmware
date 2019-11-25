cc56cb2d3d14f3539a484602f3ae2cc161eb833a
Tom Saleeba <tom.saleeba@gmail.com>
2019-11-25 22:30:16 +1030

    feat: add macro that matches my 25 layout

--- keyboards/40percentclub/25/keymaps/tomsaleeba/keymap.c
+++ keyboards/40percentclub/25/keymaps/tomsaleeba/keymap.c
@@ -1,46 +1,47 @@
 #include QMK_KEYBOARD_H
 #include "tom.h"
+#include "25_tom.h"
 
 extern keymap_config_t keymap_config;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [BASE] = LAYOUT_25_wrapper(
-             BASE_L1_5COL,                                             BASE_R1_5COL,
-             BASE_L2_5COL,                                             BASE_R2_5COL,
-             BASE_L3_5COL,                                             BASE_R3_5COL,
-    KC_LEAD, KC_LALT, KC_DEL,  KC_LCTL, KC_LSFT,     KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
-    MO(NUMB),MO(MDIA),_______, KC_TAB,  KC_BSPC,     MO(BRKT),KC_ESC,  _______, KC_RSFT, GUI_ENT
+  [BASE] = LAYOUT_25_2move_wrapper(
+             BASE_L1_5COL,                                                                BASE_R1_5COL,
+             BASE_L2_5COL,                                                                BASE_R2_5COL,
+             BASE_L3_5COL,                                                                BASE_R3_5COL,
+    KC_LEAD, KC_LALT, KC_DEL,  KC_LCTL, KC_LSFT, MO(NUMB),     GUI_ENT, KC_SPC,  KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT,
+                      _______, KC_TAB,  KC_BSPC, MO(MDIA),     KC_RSFT, MO(BRKT),KC_ESC,  _______
   ),
 
-  [MDIA] = LAYOUT_25_wrapper(
-    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
-    RGB_MOD, RGB_TOG, _______, _______, KC_F11,      KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
-    RGB_VAI, _______, _______, _______, KC_ENT,      _______, _______, _______, _______, KC_MUTE,
-    RGB_VAD, RESET,   VRSN,    _______, _______,     _______, _______, KC_VOLD, KC_VOLU, _______,
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+  [MDIA] = LAYOUT_25_2move_wrapper(
+    KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                          KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,
+    RGB_MOD, RGB_TOG, _______, _______, KC_F11,                         KC_F12,  KC_HOME, KC_PGDN, KC_PGUP, KC_END,
+    RGB_VAI, _______, _______, _______, KC_ENT,                         _______, _______, _______, _______, KC_MUTE,
+    RGB_VAD, RESET,   VRSN,    _______, _______,  _______,     _______, _______, _______, KC_VOLD, KC_VOLU, _______,
+                      _______, _______, _______,  _______,     _______, _______, _______, _______
   ),
 
-  [NUMB] = LAYOUT_25_wrapper(
-             NUMB_L1_5COL,                                             NUMB_R1_5COL,
-             NUMB_L2_5COL,                                             NUMB_R2_5COL,
-             NUMB_L3_5COL,                                             NUMB_R3_5COL,
-    _______, _______, _______, _______, _______,                       NUMB_R4_4COL,     _______,
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+  [NUMB] = LAYOUT_25_2move_wrapper(
+             NUMB_L1_5COL,                                                                NUMB_R1_5COL,
+             NUMB_L2_5COL,                                                                NUMB_R2_5COL,
+             NUMB_L3_5COL,                                                                NUMB_R3_5COL,
+    _______, _______, _______, _______, _______,  _______,     _______, _______,          NUMB_R4_4COL,
+                      _______, _______, _______,  _______,     _______, _______, _______, _______
   ),
 
-  [MOUS] = LAYOUT_25_wrapper(
-             MOUS_L1_5COL,                                             MOUS_R1_5COL,
-             MOUS_L2_5COL,                                             MOUS_R2_5COL,
-             MOUS_L3_5COL,                                             MOUS_R3_5COL,
-    _______, _______, _______, _______, KC_ACL0,     KC_BTN1, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, KC_BTN2
+  [MOUS] = LAYOUT_25_2move_wrapper(
+             MOUS_L1_5COL,                                                                MOUS_R1_5COL,
+             MOUS_L2_5COL,                                                                MOUS_R2_5COL,
+             MOUS_L3_5COL,                                                                MOUS_R3_5COL,
+    _______, _______, _______, _______, KC_ACL0,  _______,     KC_BTN2, KC_BTN1, _______, _______, _______, _______,
+                      _______, _______, _______,  _______,     _______, _______, _______, _______
   ),
 
-  [BRKT] = LAYOUT_25_wrapper(
-             BRKT_L1_5COL,                                             BRKT_R1_5COL,
-             BRKT_L2_5COL,                                             BRKT_R2_5COL,
-             BRKT_L3_5COL,                                             BRKT_R3_5COL,
-    _______, _______, _______, _______, _______,     _______, KC_LT,   KC_GT,   _______, _______,
-    _______, _______, _______, _______, _______,     _______, _______, _______, _______, _______
+  [BRKT] = LAYOUT_25_2move_wrapper(
+             BRKT_L1_5COL,                                                                BRKT_R1_5COL,
+             BRKT_L2_5COL,                                                                BRKT_R2_5COL,
+             BRKT_L3_5COL,                                                                BRKT_R3_5COL,
+    _______, _______, _______, _______, _______,  _______,     _______, _______, KC_LT,   KC_GT,   _______, _______,
+                      _______, _______, _______,  _______,     _______, _______, _______, _______
   ),
 };
