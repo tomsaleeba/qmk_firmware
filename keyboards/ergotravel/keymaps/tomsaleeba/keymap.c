fe476b0066fedd7d103a8e36ccf58a01baa1bfbc
Tom Saleeba <tom.saleeba@gmail.com>
2019-12-01 22:47:58 +1030

    feat: update map for ergotravel and add layer led

--- keyboards/ergotravel/keymaps/tomsaleeba/keymap.c
+++ keyboards/ergotravel/keymaps/tomsaleeba/keymap.c
@@ -6,10 +6,10 @@ extern keymap_config_t keymap_config;
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [BASE] = LAYOUT_ergotravel_grid_wrapper(
-   _______,          BASE_L1_5COL,                       KC_TAB,            RGB_TOG,                   BASE_R1_5COL,              _______,
+   _______,          BASE_L1_5COL,                       MO(BRKT),          RGB_TOG,                   BASE_R1_5COL,              _______,
    _______,          BASE_L2_5COL,                       MO(MDIA),          RESET,                     BASE_R2_5COL,              _______,
-   _______,          BASE_L3_5COL,                       MO(NUMB),          GUI_ENT,                   BASE_R3_5COL,              _______,
-   _______, KC_LEAD, KC_LALT, KC_DEL,           CTL_ESC, KC_LSFT,           KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______
+   _______,          BASE_L3_5COL,                       KC_LCTL,           NMB_ENT,                   BASE_R3_5COL,              _______,
+   KC_ESC,  KC_LEAD, KC_LALT, KC_DEL,           GUI_ENT, KC_LSFT,           KC_SPC,           KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, _______
   ),
 
   [MDIA] = LAYOUT(
@@ -33,11 +33,39 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    _______, _______, _______, _______,          KC_ACL2, _______,          KC_BTN1,  _______,          _______, _______, _______, _______
   ),
 
-  [BRKT] = LAYOUT(
+  [BRKT] = LAYOUT_ergotravel_grid_wrapper(
    _______,          BRKT_L1_5COL,                       _______,          _______,                    BRKT_R1_5COL,              _______,
    _______,          BRKT_L2_5COL,                       _______,          _______,                    BRKT_R2_5COL,              _______,
    _______,          BRKT_L3_5COL,                       _______,          _______,                    BRKT_R3_5COL,              _______,
    _______, _______, _______, _______,          _______, _______,          _______,  KC_LT,            KC_GT,   _______, _______, _______
   )
-
 };
+
+#if defined(RGBLIGHT_ENABLE)
+uint32_t layer_state_set_user(uint32_t state) {
+  uint8_t layer = biton32(state);
+  switch (layer) {
+      case BASE:
+          rgblight_sethsv(HSV_TOM_ORANGE);
+        break;
+      case MDIA:
+          rgblight_sethsv(HSV_TOM_RED);
+        break;
+      case NUMB:
+          rgblight_sethsv(HSV_TOM_GREEN);
+        break;
+      case MOUS:
+          rgblight_sethsv(HSV_TOM_CYAN);
+        break;
+      case BRKT:
+          rgblight_sethsv(HSV_TOM_PURPLE);
+        break;
+      case SWAP:
+          rgblight_sethsv(HSV_TOM_WHITE);
+        break;
+      default:
+        break;
+    }
+  return state;
+};
+#endif
