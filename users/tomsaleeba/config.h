ada3208d5117365671aeb0275b76f1521f1ee996
Tom Saleeba <tom.saleeba@gmail.com>
2019-12-05 17:43:22 +1030

    tighten up combo_term even more, I'm accidentally triggering it sometimes

--- users/tomsaleeba/config.h
+++ users/tomsaleeba/config.h
@@ -43,7 +43,7 @@
 
 #define COMBO_COUNT 19
 #undef COMBO_TERM
-#define COMBO_TERM 22
+#define COMBO_TERM 13
 
 #undef TAPPING_TERM_PER_KEY
 // https://github.com/qmk/qmk_firmware/blob/master/docs/custom_quantum_functions.md#custom-tapping-term
